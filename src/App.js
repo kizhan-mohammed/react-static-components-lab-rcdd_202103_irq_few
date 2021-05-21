59bd21c1d58a2279709a63d73bba8fcdcd2a9c43
Kizhan Mohammed <kizhan.m.kareem@gmail.com>
2021-05-21 12:47:14 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
+<<<<<<< HEAD
 import CatComponent from './CatComponent';
 import{GraceHopperQuoteComponent} from './GraceHopperQuoteComponent';
 import MouseComponent from './MouseComponent';
+=======
+import CatComponent from './src/CatComponent.js'
+import {GraceHopperQuoteComponent} from './src/GraceHopperQuoteComponent';
+import MouseComponent from './src/MouseComponent.js'
+>>>>>>> 8f638ce77f51542fae1b72031fb0541972b4bb4e
 
 
 
